8edffde9c156bf90b5f57593fa1176f7e0ed618c
Fatih Arslan <ftharsln@gmail.com>
2021-01-09 14:15:18 +0300

    Extracted from github/planetscale/cli/psapi

---
+++ client.go
@@ -0,0 +1,182 @@
+package planetscale
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/hashicorp/go-cleanhttp"
+	"golang.org/x/oauth2"
+)
+
+const (
+	DefaultBaseURL = "https://api.planetscaledb.io/"
+	jsonMediaType  = "application/json"
+)
+
+// Client encapsulates a client that talks to the PlanetScale API
+type Client struct {
+	client *http.Client
+
+	// Base URL for the API
+	BaseURL *url.URL
+
+	Databases DatabasesService
+}
+
+// ClientOption provides a variadic option for configuring the client
+type ClientOption func(c *Client) error
+
+// SetBaseURL overrides the base URL for the API.
+func SetBaseURL(baseURL string) ClientOption {
+	return func(c *Client) error {
+		parsedURL, err := url.Parse(baseURL)
+		if err != nil {
+			return err
+		}
+
+		c.BaseURL = parsedURL
+		return nil
+	}
+}
+
+// NewClient instantiates an instance of the PlanetScale API client
+func NewClient(client *http.Client, opts ...ClientOption) (*Client, error) {
+	if client == nil {
+		client = cleanhttp.DefaultClient()
+	}
+
+	baseURL, err := url.Parse(DefaultBaseURL)
+	if err != nil {
+		return nil, err
+	}
+	c := &Client{
+		client:  client,
+		BaseURL: baseURL,
+	}
+
+	for _, opt := range opts {
+		err := opt(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	c.Databases = &databasesService{
+		client: c,
+	}
+
+	return c, nil
+}
+
+// NewClientFromToken instantiates an API client with a given access token.
+func NewClientFromToken(accessToken string, opts ...ClientOption) (*Client, error) {
+	if accessToken == "" {
+		return nil, errors.New("missing access token")
+	}
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
+	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+
+	return NewClient(oauthClient, opts...)
+}
+
+// GetAPIEndpoint simply returns an API endpoint.
+func (c *Client) GetAPIEndpoint(path string) string {
+	return fmt.Sprintf("%s/%s", c.BaseURL, path)
+}
+
+// Do executes the inputted HTTP request.
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	req = req.WithContext(ctx)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		out, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		errorRes := &ErrorResponse{}
+		err = json.Unmarshal(out, errorRes)
+		if err != nil {
+			return nil, err
+		}
+
+		// json.Unmarshal doesn't return an error if the response
+		// body has a different protocol then "ErrorResponse". We
+		// check here to make sure that errorRes is populated. If
+		// not, we return the full response back to the user, so
+		// they can debug the issue.
+		// TODO(arslan): fix the behavior on the API side
+		if *errorRes == (ErrorResponse{}) {
+			return nil, errors.New(string(out))
+		}
+
+		return res, errorRes
+	}
+
+	if v != nil {
+		err = json.NewDecoder(res.Body).Decode(v)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return res, nil
+}
+
+func (c *Client) NewRequest(method string, path string, body interface{}) (*http.Request, error) {
+	u, err := c.BaseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var req *http.Request
+	switch method {
+	case http.MethodGet:
+		req, err = http.NewRequest(method, u.String(), nil)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		buf := new(bytes.Buffer)
+		if body != nil {
+			err = json.NewEncoder(buf).Encode(body)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		req, err = http.NewRequest(method, u.String(), buf)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Content-Type", jsonMediaType)
+	}
+
+	req.Header.Set("Accept", jsonMediaType)
+
+	return req, nil
+}
+
+// ErrorResponse represents an error response from the API
+type ErrorResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e ErrorResponse) Error() string {
+	return e.Message
+}
---
+++ databases.go
@@ -0,0 +1,159 @@
+package planetscale
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+var databasesAPIPath = "databases"
+
+// CreateDatabaseRequest encapsulates the request for creating a new database.
+type CreateDatabaseRequest struct {
+	Database *Database `json:"database"`
+}
+
+// DatabaseService is an interface for communicating with the PlanetScale
+// Databases API endpoint.
+type DatabasesService interface {
+	Create(context.Context, *CreateDatabaseRequest) (*Database, error)
+	Get(context.Context, int64) (*Database, error)
+	Status(context.Context, int64) (*DatabaseStatus, error)
+	List(context.Context) ([]*Database, error)
+	Delete(context.Context, int64) (bool, error)
+}
+
+// Database represents a PlanetScale database
+type Database struct {
+	ID        int64      `json:"id,omitempty" header:"id"`
+	Name      string     `json:"name" header:"name"`
+	Notes     string     `json:"notes" header:"notes"`
+	CreatedAt *time.Time `json:"created_at" header:"created_at,unixtime_human"`
+	UpdatedAt *time.Time `json:"updated_at" header:"updated_at,unixtime_human"`
+}
+
+// DatabaseStatus represents the status of a PlanetScale database.
+type DatabaseStatus struct {
+	DatabaseID    int64  `json:"database_id" header:"database_id"`
+	DeployPhase   string `json:"deploy_phase" header:"status"`
+	GatewayHost   string `json:"mysql_gateway_host" header:"gateway_host"`
+	GatewayPort   int    `json:"mysql_gateway_port" header:"gateway_port"`
+	MySQLUser     string `json:"mysql_gateway_user" header:"user"`
+	MySQLPassword string `json:"mysql_gateway_pass" header:"password"`
+}
+
+type databasesService struct {
+	client *Client
+}
+
+var _ DatabasesService = &databasesService{}
+
+func NewDatabasesService(client *Client) *databasesService {
+	return &databasesService{
+		client: client,
+	}
+}
+
+type ListDatabasesResponse struct {
+	Databases []*Database `json:"databases"`
+}
+
+func (ds *databasesService) List(ctx context.Context) ([]*Database, error) {
+	req, err := ds.client.NewRequest(http.MethodGet, databasesAPIPath, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	listRes := &ListDatabasesResponse{}
+	_, err = ds.client.Do(ctx, req, listRes)
+	if err != nil {
+		return nil, err
+	}
+
+	return listRes.Databases, nil
+}
+
+// DatabaseResponse encapsulates the JSON returned after successfully creating
+// or fetching a database.
+type DatabaseResponse struct {
+	Database *Database `json:"database"`
+}
+
+func (ds *databasesService) Create(ctx context.Context, createReq *CreateDatabaseRequest) (*Database, error) {
+	req, err := ds.client.NewRequest(http.MethodPost, databasesAPIPath, createReq)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for create database")
+	}
+
+	createRes := &DatabaseResponse{}
+	_, err = ds.client.Do(ctx, req, createRes)
+	if err != nil {
+		return nil, err
+	}
+
+	return createRes.Database, nil
+}
+
+func (ds *databasesService) Get(ctx context.Context, id int64) (*Database, error) {
+	path := fmt.Sprintf("%s/%d", databasesAPIPath, id)
+	req, err := ds.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for get database")
+	}
+
+	_, err = ds.client.Do(ctx, req, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	dbRes := &DatabaseResponse{}
+	_, err = ds.client.Do(ctx, req, dbRes)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbRes.Database, nil
+}
+
+func (ds *databasesService) Delete(ctx context.Context, id int64) (bool, error) {
+	path := fmt.Sprintf("%s/%d", databasesAPIPath, id)
+	req, err := ds.client.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return false, errors.Wrap(err, "error creating request for delete database")
+	}
+
+	res, err := ds.client.Do(ctx, req, nil)
+	if err != nil {
+		return false, errors.Wrap(err, "error deleting database")
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return false, errors.New("database not found")
+	}
+
+	return true, nil
+}
+
+// StatusResponse returns a response for the status of a database
+type StatusResponse struct {
+	Status *DatabaseStatus `json:"status"`
+}
+
+func (ds *databasesService) Status(ctx context.Context, id int64) (*DatabaseStatus, error) {
+	path := fmt.Sprintf("%s/%d/status", databasesAPIPath, id)
+	req, err := ds.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for database status")
+	}
+
+	status := &StatusResponse{}
+	_, err = ds.client.Do(ctx, req, status)
+	if err != nil {
+		return nil, errors.Wrap(err, "error getting database status")
+	}
+
+	return status.Status, nil
+}

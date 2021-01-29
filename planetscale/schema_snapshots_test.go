a046496adc7b4022334fd32de07eecdb3ef3dbc4
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2021-01-29 13:01:24 -0500

    Get started on Schema Snapshots endpoints.

---
+++ planetscale/schema_snapshots_test.go
@@ -0,0 +1,9 @@
+package planetscale
+
+import "testing"
+
+func TestSchemaSnapshots_Create(t *testing.T) {}
+
+func TestSchemaSnapshots_Get(t *testing.T) {}
+
+func TestSchemaSnapshots_List(t *testing.T) {}

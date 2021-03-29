2ef8f4848dc1b2a2537503a34b5d4f5ed784512a
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2021-03-29 16:56:09 -0400

    Fix up tests.

--- planetscale/deploy_requests_test.go
+++ planetscale/deploy_requests_test.go
@@ -26,7 +26,9 @@ func TestDeployRequests_Get(t *testing.T) {
 	ctx := context.Background()
 
 	dr, err := client.DeployRequests.Get(ctx, &GetDeployRequestRequest{
-		ID: "test-deploy-request-id",
+		Organization: "test-organization",
+		Database:     "test-database",
+		Number:       1337,
 	})
 
 	testTime := time.Date(2021, time.January, 14, 10, 19, 23, 000, time.UTC)
@@ -61,7 +63,10 @@ func TestDeployRequests_Deploy(t *testing.T) {
 	ctx := context.Background()
 
 	dr, err := client.DeployRequests.Deploy(ctx, &PerformDeployRequest{
-		ID: "test-deploy-request-id",
+
+		Organization: "test-organization",
+		Database:     "test-database",
+		Number:       1337,
 	})
 
 	testTime := time.Date(2021, time.January, 14, 10, 19, 23, 000, time.UTC)
@@ -78,4 +83,41 @@ func TestDeployRequests_Deploy(t *testing.T) {
 
 	c.Assert(err, qt.IsNil)
 	c.Assert(dr, qt.DeepEquals, want)
+}
+
+func TestDeployRequests_List(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"data": [{"id": "test-deploy-request-id", "branch": "development", "into_branch": "some-branch", "notes": "", "created_at": "2021-01-14T10:19:23.000Z", "updated_at": "2021-01-14T10:19:23.000Z", "closed_at": "2021-01-14T10:19:23.000Z"}]}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+
+	requests, err := client.DeployRequests.List(ctx, &ListDeployRequestsRequest{
+		Organization: testOrg,
+		Database:     testDatabase,
+	})
+
+	testTime := time.Date(2021, time.January, 14, 10, 19, 23, 000, time.UTC)
+
+	want := []*DeployRequest{{
+		ID:         "test-deploy-request-id",
+		Branch:     "development",
+		IntoBranch: "some-branch",
+		Notes:      "",
+		CreatedAt:  testTime,
+		UpdatedAt:  testTime,
+		ClosedAt:   &testTime,
+	},
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(requests, qt.DeepEquals, want)
 }

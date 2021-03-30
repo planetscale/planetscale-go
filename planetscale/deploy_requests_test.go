2995f7e94944d57fc767c8fea8f01dc81baf20b5
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2021-03-30 18:23:50 -0400

    More test fixups.

--- planetscale/deploy_requests_test.go
+++ planetscale/deploy_requests_test.go
@@ -15,7 +15,7 @@ func TestDeployRequests_Get(t *testing.T) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
-		out := `{"id": "test-deploy-request-id", "branch": "development", "into_branch": "some-branch", "notes": "", "created_at": "2021-01-14T10:19:23.000Z", "updated_at": "2021-01-14T10:19:23.000Z", "closed_at": null}`
+		out := `{"id": "test-deploy-request-id", "branch": "development", "into_branch": "some-branch", "notes": "", "created_at": "2021-01-14T10:19:23.000Z", "updated_at": "2021-01-14T10:19:23.000Z", "closed_at": null, "number": 1337}`
 		_, err := w.Write([]byte(out))
 		c.Assert(err, qt.IsNil)
 	}))
@@ -35,6 +35,7 @@ func TestDeployRequests_Get(t *testing.T) {
 
 	want := &DeployRequest{
 		ID:         "test-deploy-request-id",
+		Number:     1337,
 		Branch:     "development",
 		IntoBranch: "some-branch",
 		Notes:      "",
@@ -163,6 +164,44 @@ func TestDeployRequests_Close(t *testing.T) {
 
 	c.Assert(err, qt.IsNil)
 	c.Assert(dr, qt.DeepEquals, want)
+}
+
+func TestDeployRequests_Create(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"id": "test-deploy-request-id", "number": 1337, "branch": "development", "into_branch": "some-branch", "notes": "", "created_at": "2021-01-14T10:19:23.000Z", "updated_at": "2021-01-14T10:19:23.000Z", "closed_at": "2021-01-14T10:19:23.000Z"}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+
+	requests, err := client.DeployRequests.Create(ctx, &CreateDeployRequestRequest{
+		Organization: testOrg,
+		Database:     testDatabase,
+		Notes:        "",
+	})
+
+	testTime := time.Date(2021, time.January, 14, 10, 19, 23, 000, time.UTC)
+
+	want := &DeployRequest{
+		ID:         "test-deploy-request-id",
+		Number:     1337,
+		Branch:     "development",
+		IntoBranch: "some-branch",
+		Notes:      "",
+		CreatedAt:  testTime,
+		UpdatedAt:  testTime,
+		ClosedAt:   &testTime,
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(requests, qt.DeepEquals, want)
 }
 
 func TestDeployRequests_List(t *testing.T) {

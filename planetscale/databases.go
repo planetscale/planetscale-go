a14f5da53e5a391f452e3b47a8fafba802bf25f4
Phani Raj <phani@planetscale.com>
2022-05-10 14:32:19 -0500

    add types and DataImportService to access Data Imports API

--- planetscale/databases.go
+++ planetscale/databases.go
@@ -59,13 +59,14 @@ const (
 
 // Database represents a PlanetScale database
 type Database struct {
-	Name      string        `json:"name"`
-	Notes     string        `json:"notes"`
-	Region    Region        `json:"region"`
-	State     DatabaseState `json:"state"`
-	HtmlURL   string        `json:"html_url"`
-	CreatedAt time.Time     `json:"created_at"`
-	UpdatedAt time.Time     `json:"updated_at"`
+	Name       string        `json:"name"`
+	Notes      string        `json:"notes"`
+	Region     Region        `json:"region"`
+	State      DatabaseState `json:"state"`
+	HtmlURL    string        `json:"html_url"`
+	CreatedAt  time.Time     `json:"created_at"`
+	UpdatedAt  time.Time     `json:"updated_at"`
+	DataImport DataImport    `json:"data_import"`
 }
 
 // Database represents a list of PlanetScale databases
--- planetscale/imports.go
+++ planetscale/imports.go
@@ -10,7 +10,7 @@ import (
 
 type DataImportSource struct {
 	HostName            string `json:"hostname"`
-	Database            string `json:"database"`
+	Database            string `json:"schema_name"`
 	Port                int    `json:"port"`
 	SSLVerificationMode string `json:"ssl_mode"`
 	UserName            string `json:"username"`
@@ -20,39 +20,113 @@ type DataImportSource struct {
 type DataImportState int
 
 const (
-	PreparingDataCopy DataImportState = iota
-	PreparingDataCopyFailed
-	CopyingData
-	CopyingDataFailed
-	SwitchTrafficPending
-	SwitchTrafficRunning
-	SwitchTrafficCompleted
-	SwitchTrafficError
-	ReverseTrafficRunning
-	ReverseTrafficCompleted
-	ReverseTrafficError
-	DetachExternalDatabaseRunning
-	DetachExternalDatabaseError
+	DataImportPreparingDataCopy DataImportState = iota
+	DataImportPreparingDataCopyFailed
+	DataImportCopyingData
+	DataImportCopyingDataFailed
+	DataImportSwitchTrafficPending
+	DataImportSwitchTrafficRunning
+	DataImportSwitchTrafficCompleted
+	DataImportSwitchTrafficError
+	DataImportReverseTrafficRunning
+	DataImportReverseTrafficCompleted
+	DataImportReverseTrafficError
+	DataImportDetachExternalDatabaseRunning
+	DataImportDetachExternalDatabaseError
 	Ready
 )
+
+func (d DataImportState) String() string {
+	switch d {
+	case DataImportPreparingDataCopy:
+		return "preparing to copy data from external database"
+	case DataImportPreparingDataCopyFailed:
+		return "failed to copy data from external database"
+	case DataImportCopyingData:
+		return "copying data from external database"
+	case DataImportCopyingDataFailed:
+		return "failed to copy data from external database"
+	case DataImportSwitchTrafficPending:
+		return "PlanetScale database is running in replica mode"
+	case DataImportSwitchTrafficRunning:
+		return "switching PlanetScale database to primary mode"
+	case DataImportSwitchTrafficError:
+		return "failed to switching PlanetScale database to primary mode"
+	case DataImportReverseTrafficRunning:
+		return "switching PlanetScale database to replica mode"
+	case DataImportReverseTrafficError:
+		return "failed to switching PlanetScale database to replica mode"
+	case DataImportDetachExternalDatabaseRunning:
+		return "detaching external database from  PlanetScale database"
+	case DataImportDetachExternalDatabaseError:
+		return "failed to detach external database from  PlanetScale database"
+	}
+
+	return "all done"
+}
 
 type DataImport struct {
 	ID          string `json:"id"`
 	ImportState DataImportState
+	State       string    `json:"state"`
 	Errors      string    `json:"import_check_errors"`
 	StartedAt   time.Time `json:"started_at"`
 	FinishedAt  time.Time `json:"finished_at"`
 	DeleteddAt  time.Time `json:"deleted_at"`
 }
 
+func (di *DataImport) ParseState() {
+	switch di.State {
+	case "prepare_data_copy_pending":
+		di.ImportState = DataImportPreparingDataCopy
+		break
+	case "prepare_data_copy_error":
+		di.ImportState = DataImportPreparingDataCopyFailed
+		break
+	case "data_copy_pending":
+		di.ImportState = DataImportCopyingData
+		break
+	case "data_copy_error":
+		di.ImportState = DataImportPreparingDataCopyFailed
+		break
+	case "switch_traffic_workflow_pending":
+		di.ImportState = DataImportSwitchTrafficPending
+		break
+	case "switch_traffic_workflow_running":
+		di.ImportState = DataImportSwitchTrafficRunning
+		break
+	case "switch_traffic_workflow_error":
+		di.ImportState = DataImportSwitchTrafficError
+		break
+	case "reverse_traffic_workflow_running":
+		di.ImportState = DataImportReverseTrafficRunning
+		break
+	case "reverse_traffic_workflow_error":
+		di.ImportState = DataImportReverseTrafficError
+		break
+	case "cleanup_workflow_pending":
+		di.ImportState = DataImportSwitchTrafficCompleted
+		break
+	case "cleanup_workflow_running":
+		di.ImportState = DataImportDetachExternalDatabaseRunning
+		break
+	case "cleanup_workflow_error":
+		di.ImportState = DataImportDetachExternalDatabaseError
+		break
+	default:
+		di.ImportState = Ready
+	}
+}
+
 type TestDataImportSourceRequest struct {
 	Organization string           `json:"organization"`
+	Database     string           `json:"name"`
 	Source       DataImportSource `json:"connection"`
 }
 
-// PromotionRequestLintError represents an error that occurs during branch
-// promotion.
-type DataSourceIncompatiblityError struct {
+// DataSourceIncompatibilityError represents an error that occurs when the
+// source schema in an external database server is incompatible with PlanetScale.
+type DataSourceIncompatibilityError struct {
 	LintError        string `json:"lint_error"`
 	Keyspace         string `json:"keyspace_name"`
 	Table            string `json:"table_name"`
@@ -62,40 +136,61 @@ type DataSourceIncompatiblityError struct {
 }
 
 type TestDataImportSourceResponse struct {
-	CanConnect   bool                             `json:"can_connect"`
-	ConnectError string                           `json:"error"`
-	Errors       []*DataSourceIncompatiblityError `json:"lint_errors"`
+	CanConnect   bool                              `json:"can_connect"`
+	ConnectError string                            `json:"error"`
+	Errors       []*DataSourceIncompatibilityError `json:"lint_errors"`
 }
 
-type CreateDataImportRequest struct {
-	Organization string           `json:"organization"`
+type StartDataImportRequest struct {
 	DatabaseName string           `json:"name"`
+	Organization string           `json:"organization"`
 	Source       DataImportSource `json:"connection"`
 }
 
 type MakePlanetScalePrimaryRequest struct {
 	Organization string `json:"organization"`
-	DatabaseName string `json:"name"`
+	Database     string `json:"name"`
 }
 
 type MakePlanetScaleReplicaRequest struct {
 	Organization string `json:"organization"`
-	DatabaseName string `json:"name"`
+	Database     string `json:"name"`
 }
 
-// DatabaseBranchPasswordsService is an interface for communicating with the PlanetScale
-// Database Branch Passwords API endpoint.
+type DetachExternalDatabaseRequest struct {
+	Organization string `json:"organization"`
+	Database     string `json:"name"`
+}
+
+type GetImportStatusRequest struct {
+	Organization string `json:"organization"`
+	Database     string `json:"name"`
+}
+
+type CancelDataImportRequest struct {
+	Organization string `json:"organization"`
+	Database     string `json:"name"`
+}
+
+// DataImportsService is an interface for communicating with the PlanetScale
+// Data Imports API endpoint.
 type DataImportsService interface {
-	TestDataImportSource(ctx context.Context, request TestDataImportSourceRequest) (*TestDataImportSourceResponse, error)
+	TestDataImportSource(ctx context.Context, request *TestDataImportSourceRequest) (*TestDataImportSourceResponse, error)
+	StartDataImport(ctx context.Context, request *StartDataImportRequest) (*DataImport, error)
+	CancelDataImport(ctx context.Context, request *CancelDataImportRequest) (*DataImport, error)
+	GetDataImportStatus(ctx context.Context, request *GetImportStatusRequest) (*DataImport, error)
+	MakePlanetScalePrimary(ctx context.Context, request *MakePlanetScalePrimaryRequest) (*DataImport, error)
+	MakePlanetScaleReplica(ctx context.Context, request *MakePlanetScaleReplicaRequest) (*DataImport, error)
+	DetachExternalDatabase(ctx context.Context, request *DetachExternalDatabaseRequest) (*DataImport, error)
 }
 
 type dataImportsService struct {
 	client *Client
 }
 
-// Creates a new password for a branch.
-func (d *dataImportsService) TestDataImportSource(ctx context.Context, request TestDataImportSourceRequest) (*TestDataImportSourceResponse, error) {
-	path := fmt.Sprintf("/organizations/%s/data-imports/test-connection", request.Organization)
+// TestDataImportSource will check an external database for compatibility with PlanetScale
+func (d *dataImportsService) TestDataImportSource(ctx context.Context, request *TestDataImportSourceRequest) (*TestDataImportSourceResponse, error) {
+	path := fmt.Sprintf("/v1/organizations/%s/data-imports/test-connection", request.Organization)
 	req, err := d.client.newRequest(http.MethodPost, path, request)
 	if err != nil {
 		return nil, errors.Wrap(err, "error creating http request")
@@ -107,4 +202,104 @@ func (d *dataImportsService) TestDataImportSource(ctx context.Context, request T
 	}
 
 	return resp, nil
+}
+
+func (d *dataImportsService) StartDataImport(ctx context.Context, request *StartDataImportRequest) (*DataImport, error) {
+	path := fmt.Sprintf("/v1/organizations/%s/data-imports/new", request.Organization)
+	req, err := d.client.newRequest(http.MethodPost, path, request)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	resp := &DataImport{}
+	if err := d.client.do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (d *dataImportsService) GetDataImportStatus(ctx context.Context, getReq *GetImportStatusRequest) (*DataImport, error) {
+	path := fmt.Sprintf("/v1/organizations/%s/databases/%s", getReq.Organization, getReq.Database)
+	req, err := d.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating request for get database")
+	}
+
+	db := &Database{}
+	err = d.client.do(ctx, req, &db)
+	if err != nil {
+		return nil, err
+	}
+
+	if db.DataImport.ID == "" {
+		return nil, errors.Errorf("Database %s is not importing data", getReq.Database)
+	}
+
+	db.DataImport.ParseState()
+	return &db.DataImport, nil
+}
+
+func (d *dataImportsService) CancelDataImport(ctx context.Context, cancelReq *CancelDataImportRequest) (*DataImport, error) {
+	path := fmt.Sprintf("%s/cancel", dataImportAPIPath(cancelReq.Organization, cancelReq.Database))
+	req, err := d.client.newRequest(http.MethodPost, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	resp := &DataImport{}
+	if err := d.client.do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+func (d *dataImportsService) MakePlanetScalePrimary(ctx context.Context, request *MakePlanetScalePrimaryRequest) (*DataImport, error) {
+	path := fmt.Sprintf("%s/begin-switch-traffic", dataImportAPIPath(request.Organization, request.Database))
+	req, err := d.client.newRequest(http.MethodPost, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	resp := &DataImport{}
+	if err := d.client.do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+	resp.ParseState()
+	return resp, nil
+}
+
+func (d *dataImportsService) MakePlanetScaleReplica(ctx context.Context, request *MakePlanetScaleReplicaRequest) (*DataImport, error) {
+	path := fmt.Sprintf("%s/begin-reverse-traffic", dataImportAPIPath(request.Organization, request.Database))
+	req, err := d.client.newRequest(http.MethodPost, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	resp := &DataImport{}
+	if err := d.client.do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+	resp.ParseState()
+	return resp, nil
+}
+
+func (d *dataImportsService) DetachExternalDatabase(ctx context.Context, request *DetachExternalDatabaseRequest) (*DataImport, error) {
+	path := fmt.Sprintf("%s/cleanup", dataImportAPIPath(request.Organization, request.Database))
+	req, err := d.client.newRequest(http.MethodPost, path, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating http request")
+	}
+
+	resp := &DataImport{}
+	if err := d.client.do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+
+	resp.ParseState()
+	return resp, nil
+}
+
+func dataImportAPIPath(organization, database string) string {
+	return fmt.Sprintf("/v1/organizations/%s/databases/%s/data-imports", organization, database)
 }

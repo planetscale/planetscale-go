591075bcab47d904b0fc70d6c8a4febc7a157a5f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-07-07 17:39:13 -0500

    Scaffold out rest of the PostgresBranchesService

--- planetscale/postgres_branches.go
+++ planetscale/postgres_branches.go
@@ -22,8 +22,6 @@ type DeletePostgresBranchRequest struct{}
 
 type PostgresBranchSchemaRequest struct{}
 
-type ListPostgresBranchClusterSKUsRequest struct{}
-
 // PostgresBranchSchema encapsulates the schema of a Postgres branch.
 type PostgresBranchSchema struct {
 	Name string `json:"name"`
@@ -38,4 +36,45 @@ type PostgresBranchesService interface {
 	Delete(context.Context, *DeletePostgresBranchRequest) error
 	Schema(context.Context, *PostgresBranchSchemaRequest) (*PostgresBranchSchema, error)
 	ListClusterSKUs(context.Context, *ListBranchClusterSKUsRequest) ([]*ClusterSKU, error)
+}
+
+type postgresBranchesService struct {
+	client *Client
+}
+
+var _ PostgresBranchesService = &postgresBranchesService{}
+
+func NewPostgresBranchesService(client *Client) *postgresBranchesService {
+	return &postgresBranchesService{
+		client: client,
+	}
+}
+
+func (p *postgresBranchesService) Create(ctx context.Context, req *CreatePostgresBranchRequest) (*PostgresBranch, error) {
+	panic("unimplemented")
+}
+
+// Delete implements PostgresBranchesService.
+func (p *postgresBranchesService) Delete(context.Context, *DeletePostgresBranchRequest) error {
+	panic("unimplemented")
+}
+
+// Get implements PostgresBranchesService.
+func (p *postgresBranchesService) Get(context.Context, *GetPostgresBranchRequest) (*PostgresBranch, error) {
+	panic("unimplemented")
+}
+
+// List implements PostgresBranchesService.
+func (p *postgresBranchesService) List(context.Context, *ListPostgresBranchesRequest) ([]*PostgresBranch, error) {
+	panic("unimplemented")
+}
+
+// ListClusterSKUs implements PostgresBranchesService.
+func (p *postgresBranchesService) ListClusterSKUs(context.Context, *ListBranchClusterSKUsRequest) ([]*ClusterSKU, error) {
+	panic("unimplemented")
+}
+
+// Schema implements PostgresBranchesService.
+func (p *postgresBranchesService) Schema(context.Context, *PostgresBranchSchemaRequest) (*PostgresBranchSchema, error) {
+	panic("unimplemented")
 }

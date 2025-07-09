45f5992f8e4614c4d3f470557ed8d0cacc7d335c
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2025-07-09 12:22:34 -0500

    Implement `PostgresBranchesService` for Postgres branches (#257)
    * Get started on PostgresBranchesService
    
    * Use standard cluster SKU
    
    * Scaffold out rest of the PostgresBranchesService
    
    * Implement Create function for PostgresBranches
    
    * Add PostgresBranches service to Client
    
    * Update CreatePostgresBranchRequest
    
    * Update comments
    
    * Fill out LIst struct
    
    * Implement remaining interface methods
    
    * Start on tests
    
    * Implement namespace filtering on schema
    
    * Add kind and update comments
    
    * Fix Schema return type

---
+++ planetscale/postgres_branches.go
@@ -0,0 +1,225 @@
+package planetscale
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+// PostgresBranch represents a Postgres branch in the PlanetScale API.
+type PostgresBranch struct {
+	ID                  string    `json:"id"`
+	Name                string    `json:"name"`
+	ClusterName         string    `json:"cluster_name"`
+	ClusterDisplayName  string    `json:"cluster_display_name"`
+	ClusterArchitecture string    `json:"cluster_architecture"`
+	ClusterIOPS         int       `json:"cluster_iops"`
+	State               string    `json:"state"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
+	Actor               Actor     `json:"actor"`
+	Production          bool      `json:"production"`
+	Ready               bool      `json:"ready"`
+	ParentBranch        string    `json:"parent_branch"`
+	Region              Region    `json:"region"`
+	Kind                string    `json:"kind"`
+}
+
+type postgresBranchesResponse struct {
+	Branches []*PostgresBranch `json:"data"`
+}
+
+// CreatePostgresBranchRequest encapsulates the request to create a Postgres branch.
+type CreatePostgresBranchRequest struct {
+	Organization string `json:"-"`
+	Database     string `json:"-"`
+	Region       string `json:"region,omitempty"`
+	Name         string `json:"name"`
+	ParentBranch string `json:"parent_branch"`
+	BackupID     string `json:"backup_id,omitempty"`
+	ClusterName  string `json:"cluster_name,omitempty"`
+}
+
+// ListPostgresBranchesRequest encapsulates the request to list Postgres branches for a database.
+type ListPostgresBranchesRequest struct {
+	Organization string
+	Database     string
+}
+
+// GetPostgresBranchRequest encapsulates the request to get a specific Postgres branch.
+type GetPostgresBranchRequest struct {
+	Organization string
+	Database     string
+	Branch       string
+}
+
+// DeletePostgresBranchRequest encapsulates the request to delete a Postgres branch.
+type DeletePostgresBranchRequest struct {
+	Organization string
+	Database     string
+	Branch       string
+}
+
+// PostgresBranchSchemaRequest encapsulates the request to get the schema of a Postgres branch.
+type PostgresBranchSchemaRequest struct {
+	Organization string
+	Database     string
+	Branch       string
+	Namespace    string `json:"-"`
+}
+
+// PostgresBranchSchema encapsulates the schema of a Postgres branch.
+type PostgresBranchSchema struct {
+	Name string `json:"name"`
+	Raw  string `json:"raw"`
+	HTML string `json:"html"`
+}
+
+// postgresBranchSchemaResponse returns the schemas
+type postgresBranchSchemaResponse struct {
+	Schemas []*PostgresBranchSchema `json:"data"`
+}
+
+type PostgresBranchesService interface {
+	Create(context.Context, *CreatePostgresBranchRequest) (*PostgresBranch, error)
+	List(context.Context, *ListPostgresBranchesRequest) ([]*PostgresBranch, error)
+	Get(context.Context, *GetPostgresBranchRequest) (*PostgresBranch, error)
+	Delete(context.Context, *DeletePostgresBranchRequest) error
+	Schema(context.Context, *PostgresBranchSchemaRequest) ([]*PostgresBranchSchema, error)
+	ListClusterSKUs(context.Context, *ListBranchClusterSKUsRequest, ...ListOption) ([]*ClusterSKU, error)
+}
+
+type postgresBranchesService struct {
+	client *Client
+}
+
+var _ PostgresBranchesService = &postgresBranchesService{}
+
+func NewPostgresBranchesService(client *Client) *postgresBranchesService {
+	return &postgresBranchesService{
+		client: client,
+	}
+}
+
+// Create creates a new Postgres branch in the specified organization and database.
+func (p *postgresBranchesService) Create(ctx context.Context, createReq *CreatePostgresBranchRequest) (*PostgresBranch, error) {
+	path := postgresBranchesAPIPath(createReq.Organization, createReq.Database)
+	req, err := p.client.newRequest(http.MethodPost, path, createReq)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	b := &PostgresBranch{}
+	if err := p.client.do(ctx, req, b); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// List returns a list of Postgres branches for the specified organization and database.
+func (p *postgresBranchesService) List(ctx context.Context, listReq *ListPostgresBranchesRequest) ([]*PostgresBranch, error) {
+	req, err := p.client.newRequest(http.MethodGet, postgresBranchesAPIPath(listReq.Organization, listReq.Database), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	pgBranches := &postgresBranchesResponse{}
+	if err := p.client.do(ctx, req, &pgBranches); err != nil {
+		return nil, err
+	}
+
+	return pgBranches.Branches, nil
+}
+
+// Get returns a single Postgres branch for the specified organization, database, and branch.
+func (p *postgresBranchesService) Get(ctx context.Context, getReq *GetPostgresBranchRequest) (*PostgresBranch, error) {
+	path := fmt.Sprintf("%s/%s", postgresBranchesAPIPath(getReq.Organization, getReq.Database), getReq.Branch)
+	req, err := p.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	pgBranch := &PostgresBranch{}
+	if err := p.client.do(ctx, req, &pgBranch); err != nil {
+		return nil, err
+	}
+
+	return pgBranch, nil
+}
+
+// Delete deletes a Postgres branch from the specified organization and database.
+func (p *postgresBranchesService) Delete(ctx context.Context, deleteReq *DeletePostgresBranchRequest) error {
+	path := fmt.Sprintf("%s/%s", postgresBranchesAPIPath(deleteReq.Organization, deleteReq.Database), deleteReq.Branch)
+	req, err := p.client.newRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return fmt.Errorf("error creating http request: %w", err)
+	}
+
+	err = p.client.do(ctx, req, nil)
+	return err
+}
+
+// ListClusterSKUs returns a list of cluster SKUs for the specified Postgres branch.
+func (p *postgresBranchesService) ListClusterSKUs(ctx context.Context, listReq *ListBranchClusterSKUsRequest, opts ...ListOption) ([]*ClusterSKU, error) {
+	path := fmt.Sprintf("%s/cluster-size-skus", postgresBranchAPIPath(listReq.Organization, listReq.Database, listReq.Branch))
+
+	defaultOpts := defaultListOptions()
+	for _, opt := range opts {
+		err := opt(defaultOpts)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if vals := defaultOpts.URLValues.Encode(); vals != "" {
+		path += "?" + vals
+	}
+
+	req, err := p.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	clusterSKUs := []*ClusterSKU{}
+	if err := p.client.do(ctx, req, &clusterSKUs); err != nil {
+		return nil, err
+	}
+
+	return clusterSKUs, nil
+}
+
+// Schema returns the schema for the specified Postgres branch.
+func (p *postgresBranchesService) Schema(ctx context.Context, schemaReq *PostgresBranchSchemaRequest) ([]*PostgresBranchSchema, error) {
+	path := fmt.Sprintf("%s/schema", postgresBranchAPIPath(schemaReq.Organization, schemaReq.Database, schemaReq.Branch))
+	v := url.Values{}
+	if schemaReq.Namespace != "" {
+		v.Set("namespace", schemaReq.Namespace)
+	}
+
+	if vals := v.Encode(); vals != "" {
+		path += "?" + vals
+	}
+
+	req, err := p.client.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	schemas := &postgresBranchSchemaResponse{}
+	if err := p.client.do(ctx, req, &schemas); err != nil {
+		return nil, err
+	}
+
+	return schemas.Schemas, nil
+}
+
+func postgresBranchesAPIPath(org, db string) string {
+	return fmt.Sprintf("%s/%s/branches", databasesAPIPath(org), db)
+}
+
+func postgresBranchAPIPath(org, db, branch string) string {
+	return fmt.Sprintf("%s/%s", postgresBranchesAPIPath(org, db), branch)
+}
---
+++ planetscale/postgres_branches_test.go
@@ -0,0 +1,207 @@
+package planetscale
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	qt "github.com/frankban/quicktest"
+)
+
+const testPostgresBranch = "postgres-test-branch"
+
+func TestPostgresBranches_Create(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"id":"postgres-test-branch","name":"postgres-test-branch","created_at":"2021-01-14T10:19:23.000Z","updated_at":"2021-01-14T10:19:23.000Z", "region": {"slug": "us-west", "display_name": "US West"}}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+	org := "my-org"
+	name := "postgres-test-db"
+
+	branch, err := client.PostgresBranches.Create(ctx, &CreatePostgresBranchRequest{
+		Organization: org,
+		Database:     name,
+		Region:       "us-west",
+		Name:         testPostgresBranch,
+		ParentBranch: "main",
+	})
+
+	want := &PostgresBranch{
+		ID:   "postgres-test-branch",
+		Name: testPostgresBranch,
+		Region: Region{
+			Slug: "us-west",
+			Name: "US West",
+		},
+		CreatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+		UpdatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(branch, qt.DeepEquals, want)
+}
+
+func TestPostgresBranches_List(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"data":[{"id":"postgres-test-branch","name":"postgres-test-branch","created_at":"2021-01-14T10:19:23.000Z","updated_at":"2021-01-14T10:19:23.000Z"}]}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+	org := "my-org"
+	name := "postgres-test-db"
+
+	branches, err := client.PostgresBranches.List(ctx, &ListPostgresBranchesRequest{
+		Organization: org,
+		Database:     name,
+	})
+
+	want := []*PostgresBranch{{
+		ID:        "postgres-test-branch",
+		Name:      testPostgresBranch,
+		CreatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+		UpdatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+	}}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(branches, qt.DeepEquals, want)
+}
+
+func TestPostgresBranches_Get(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"id":"postgres-test-branch","name":"postgres-test-branch","created_at":"2021-01-14T10:19:23.000Z","updated_at":"2021-01-14T10:19:23.000Z"}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+	org := "my-org"
+	name := "postgres-test-db"
+
+	branch, err := client.PostgresBranches.Get(ctx, &GetPostgresBranchRequest{
+		Organization: org,
+		Database:     name,
+		Branch:       testPostgresBranch,
+	})
+
+	want := &PostgresBranch{
+		ID:        "postgres-test-branch",
+		Name:      testPostgresBranch,
+		CreatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+		UpdatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(branch, qt.DeepEquals, want)
+}
+
+func TestPostgresBranches_Schema(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"data": [{"name": "test_schema", "raw": "CREATE TABLE test...", "html": "<div>CREATE TABLE test...</div>"}]}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+
+	schemas, err := client.PostgresBranches.Schema(ctx, &PostgresBranchSchemaRequest{
+		Organization: "my-org",
+		Database:     "postgres-test-db",
+		Branch:       testPostgresBranch,
+	})
+
+	want := []*PostgresBranchSchema{
+		{
+			Name: "test_schema",
+			Raw:  "CREATE TABLE test...",
+			HTML: "<div>CREATE TABLE test...</div>",
+		},
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(schemas, qt.DeepEquals, want)
+}
+
+func TestPostgresBranches_ListClusterSKUs(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `[
+		{
+			"name": "PS_10",
+			"type": "ClusterSizeSku",
+			"display_name": "PS-10",
+			"cpu": "1/8",
+			"provider_instance_type": null,
+			"storage": null,
+			"ram": 1,
+			"enabled": true,
+			"provider": null,
+			"rate": null,
+			"replica_rate": null,
+			"default_vtgate": "VTG_5",
+			"default_vtgate_rate": null,
+			"sort_order": 1
+		}
+	]`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+
+	skus, err := client.PostgresBranches.ListClusterSKUs(ctx, &ListBranchClusterSKUsRequest{
+		Organization: "my-org",
+		Database:     "postgres-test-db",
+		Branch:       testPostgresBranch,
+	})
+
+	want := []*ClusterSKU{
+		{
+			Name:          "PS_10",
+			DisplayName:   "PS-10",
+			CPU:           "1/8",
+			Memory:        1,
+			Enabled:       true,
+			DefaultVTGate: "VTG_5",
+			SortOrder:     1,
+		},
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(skus, qt.DeepEquals, want)
+}

c12a6c66c254cc87ab733d1c42af55c01d862359
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-07-09 14:47:52 -0500

    Add ability to create database using specific Kind

--- planetscale/databases_test.go
+++ planetscale/databases_test.go
@@ -56,6 +56,49 @@ func TestDatabases_Create(t *testing.T) {
 	c.Assert(db, qt.DeepEquals, want)
 }
 
+func TestDatabases_CreatePostgres(t *testing.T) {
+	c := qt.New(t)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		out := `{"id":"planetscale-go-test-db","type":"database","name":"planetscale-go-test-db","notes":"This is a test DB created from the planetscale-go API library","created_at":"2021-01-14T10:19:23.000Z","updated_at":"2021-01-14T10:19:23.000Z", "region": { "slug": "us-west", "display_name": "US West" },"state":"ready","kind":"postgresql"}`
+		_, err := w.Write([]byte(out))
+		c.Assert(err, qt.IsNil)
+	}))
+
+	client, err := NewClient(WithBaseURL(ts.URL))
+	c.Assert(err, qt.IsNil)
+
+	ctx := context.Background()
+	org := "my-org"
+	name := "planetscale-go-test-db"
+	notes := "This is a test DB created from the planetscale-go API library"
+
+	db, err := client.Databases.Create(ctx, &CreateDatabaseRequest{
+		Organization: org,
+		Region:       "us-west",
+		Name:         name,
+		Notes:        notes,
+		Kind:         "postgressql",
+	})
+
+	want := &Database{
+		Name:  name,
+		Notes: notes,
+		State: DatabaseReady,
+		Region: Region{
+			Slug: "us-west",
+			Name: "US West",
+		},
+		CreatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+		UpdatedAt: time.Date(2021, time.January, 14, 10, 19, 23, 0, time.UTC),
+		Kind:      "postgresql",
+	}
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(db, qt.DeepEquals, want)
+}
+
 func TestDatabases_Get(t *testing.T) {
 	c := qt.New(t)
 

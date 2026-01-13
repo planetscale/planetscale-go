df15319d95742356133edb817bf9ef682a1b6414
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2026-01-13 17:04:23 -0500

    Add ttl for service tokens

--- planetscale/service_tokens.go
+++ planetscale/service_tokens.go
@@ -115,6 +115,7 @@ func (s *serviceTokenService) DeleteAccess(ctx context.Context, delReq *DeleteSe
 type CreateServiceTokenRequest struct {
 	Organization string  `json:"-"`
 	Name         *string `json:"name,omitempty"`
+	TTL          *int    `json:"ttl,omitempty"`
 }
 
 type ListServiceTokenGrantsRequest struct {
